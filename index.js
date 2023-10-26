e36b638953a3477e64c6a87f9bebe1ce78610ac9
benjaminas2018 <38001818+benjaminas2018@users.noreply.github.com>
2023-10-26 14:29:51 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { NetworkViewer } from 'network-viewer'; //npm install network-viewer
 
 class App extends React.Component {
 
   render() {
     return (
-      <div>Hello World1!</div>
+        <div>
+        {/* <h2>Snapshot for Failure</h2> */}
+        <img src="./test.jpg" alt="Failure" />
+        <h2>Logs</h2>
+        <NetworkViewer
+            file="./analytics-eu.cloud.com.har"
+            scrollRequestPosition="near"
+            options={{
+              showTimeline: false,
+              showImportHAR: false,
+            }}/>
+      </div>
     )
   }
 }
